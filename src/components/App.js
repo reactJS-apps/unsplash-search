8b8762f2541d67657e07e3197bc08627b2caf515
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-08-26 16:49:23 +0500

    fetch images from unsplash API on request and display them temporarily in img tag

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,12 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = {
+    images: [],
+  };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
         query: term,
       },
@@ -12,11 +16,23 @@ class App extends Component {
         Authorization: 'Client-ID RLBCRw37Dlnrd4RDw6RpklMa-lZB010FHiY564ff7Ww',
       },
     });
-  }
+    this.setState({ images: response.data.results });
+    console.log(response.data.results);
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        {this.state.images.map((image) => {
+          return (
+            <img
+              src={image.urls.regular}
+              key={image.id}
+              alt={image.alt_description}
+            />
+          );
+        })}
       </div>
     );
   }
