60e2e78e1308b26cb837797d8b4d898289110b30
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-08-26 21:52:29 +0500

    assign dynamic values to specific DOM element using react refs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,17 +4,27 @@ class ImageCard extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      spans: 0,
+    };
+
     this.imageRef = createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageRef.current.clientHeight);
+    this.imageRef.current.addEventListener('load', this.setSpans);
   }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
 
   render() {
     const { urls, alt_description } = this.props.data;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} src={urls.regular} alt={alt_description} />
       </div>
     );
