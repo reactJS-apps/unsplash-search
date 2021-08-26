6fa85bb09f0ed737d58f8d34612c0b0e6c87b1d1
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-08-26 18:39:59 +0500

    apply CSS grid to organize images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ImageList.css';
 
 const ImageList = ({ images }) => {
   const photos = images.map(({ urls, id, alt_description }) => {
     return <img src={urls.regular} key={id} alt={alt_description} />;
   });
-  return <div>{photos}</div>;
+  return <div className="image-list">{photos}</div>;
 };
 
 export default ImageList;
